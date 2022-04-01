2359caea5f5e64222fa39592c2466823c3e096c5
Guilherme Abranches <guiabranches@outlook.com>
2022-04-01 18:51:31 -0300

    fixed ratio of canvas

--- sketch.js
+++ sketch.js
@@ -1,25 +1,25 @@
-const aasdasdad = '         .:░▒▓█';
-const density = 'Ñ@#W$9876543210?!abc;:+=-,._';
+// const density = "█▓▒░:.  ";
+const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
 //Shader ASCII
 
-let pic;
+var pic;
+var canvasWidth;
+var canvasHeight;
 
 function preload(){
   pic = loadImage("pics/even-lower-res.png")
 }
 
 function setup(){
-  createCanvas(1800, 1800);
-}
-
-function draw(){
+  GetCanvasHeightNWidth();
+  createCanvas(canvasWidth, canvasHeight);
   background(0);
   //image(pic, 0, 0, width, height);
 
   let w = width / pic.width;
   let h = height / pic.height;
   pic.loadPixels();
-  for(let i = 0; i < pic.width; i++){
+  for(let i = 0; i < pic.height; i++){
     for(let j = 0; j < pic.width; j++){
       const pixelIndex = (i + j * pic.width) * 4;
       const r = pic.pixels[pixelIndex + 0];
@@ -30,15 +30,28 @@ function draw(){
 
 
       noStroke();
-      // fill(avg); // Here you can add opacity as well
+      // fill(avg);
       fill(255);
 
       const len = density.length;
-      const charIndex = floor(map(avg, 0, 255, len, 0));
-      // square(i*w, j*h, w);
+      const charIndex = floor(map(avg,0,255,len,0));
+
       textSize(w);
       textAlign(CENTER, CENTER);
-      text(density.charAt(charIndex), i * w + w * .5, j * h + h * .5);
+      text(density.charAt(charIndex), i * w + w * 0.5, j * h + h * 0.5);
     }
+  }
+}
+
+function GetCanvasHeightNWidth(){
+  let windowAR = window.innerWidth/window.innerHeight;
+  let picAR = pic.width/pic.height;
+
+  console.log(windowAR);
+  console.log(picAR);
+
+  if(windowAR > picAR){
+    canvasHeight = window.innerHeight;
+    canvasWidth = pic.width * window.innerHeight/pic.height;
   }
 }
