8e91243f95ce8d4d9e925dfb224f6fde297f4730
Guilherme Abranches <guiabranches@outlook.com>
2022-04-01 17:30:06 -0300

    Version with opacity

--- sketch.js
+++ sketch.js
@@ -1,10 +1,40 @@
 const aasdasdad = '         .:░▒▓█';
 const density = 'Ñ@#W$9876543210?!abc;:+=-,._';
+//Shader ASCII
+
+let pic;
+
+function preload(){
+  pic = loadImage("pics/even-lower-res.png")
+}
 
 function setup(){
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 function draw(){
-  background(220);
+  background(0);
+  //image(pic, 0, 0, width, height);
+
+  let w = width / pic.width;
+  let h = height / pic.height;
+  pic.loadPixels();
+  for(let i = 0; i < pic.width; i++){
+    for(let j = 0; j < pic.width; j++){
+      const pixelIndex = (i + j * pic.width) * 4;
+      const r = pic.pixels[pixelIndex + 0];
+      const g = pic.pixels[pixelIndex + 1];
+      const b = pic.pixels[pixelIndex + 2];
+
+      const avg = (r+g+b)/3;
+
+
+      noStroke();
+      fill(avg);
+      // square(i*w, j*h, w);
+      textSize(w);
+      textAlign(CENTER, CENTER);
+      text('G', i * w + w * .5, j * h + h * .5);
+    }
+  }
 }
